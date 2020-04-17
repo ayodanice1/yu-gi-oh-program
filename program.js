8642b71955661b512eecc340147ade1c2e1e5b8d
ayodele.lawal <ayodanice1@gmail.com>
2020-04-17 17:49:35 +0100

    finished work

---
+++ program.js
@@ -0,0 +1,23 @@
+// expected return value
+const output = [];
+
+const myProgram = integer => {
+  for (let i =1; i <= integer; i++) {
+    if (i % 2 == 0) {
+      if (i % 3 == 0) {
+        if (i % 5 == 0) {
+          output.push('yu-gi-oh');
+        } else output.push('yu-gi');
+      } else if (i % 5 == 0) {
+        output.push('yu-oh');
+      } else output.push('yu');
+    } else if (i % 3 == 0) {
+      output.push('gi');
+    } else if (i % 5 == 0) {
+      output.push('oh');
+    } else output.push(i);
+  }
+  return output;
+}
+// use the function
+console.log(myProgram(15));
